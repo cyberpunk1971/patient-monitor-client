c8fccdd9dbe0f77450a73801d18195e81c0af73b
cyberpunk1971 <adt71@cox.net>
2020-12-24 17:10:35 -0700

    addes some styling to Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,24 @@
 import React, { useReducer, useCallback } from 'react';
 
+import Button from './UI/buttons/Button';
+import classes from './Home.module.css';
 const Home = () => {
-    return(
-        <div>HOME</div>
+    return (
+        <>
+            <div className={classes.Home}>
+                <h1 className={classes.Top_h1}>Patient Monitor</h1>
+                <h2 className={classes.Top_h2}>Designed with the healthcare provider in mind</h2>
+                <div className={classes.Container}>
+                    <section className={classes.Blurb}>
+                        <p className={classes.Home_p}>Patient Monitor allows doctors, nurses, and allied health professionals to manage critical patient data with ease.</p>
+                        <p className={classes.Home_p}>To demonstrate the function of the application, simply click the DEMO button below. To access the full suite of Patient Monitor capabilities, such as data persistence over time, please register a new account. If you are already registered, please login.</p>
+                    </section>
+                </div>
+            </div>
+            <div className={classes.Demo_div}>
+                <Button className={classes.button}>DEMO</Button>
+            </div>
+        </>
     )
 }
 
--- src/components/UI/buttons/Button.js
+++ src/components/UI/buttons/Button.js
@@ -29,8 +29,8 @@ const Button = props => {
   }
   return (
     <button
-      className={`button button--${props.size || 'default'} ${props.inverse &&
-        'button--inverse'} ${props.danger && 'button--danger'}`}
+      // className={`button button--${props.size || 'default'} ${props.inverse &&
+      //   'button--inverse'} ${props.danger && 'button--danger'}`}
       type={props.type}
       onClick={props.onClick}
       disabled={props.disabled}
